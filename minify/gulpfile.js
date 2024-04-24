bdec8f98e9e39ed93b3fbf39c1cce8c6b48f337d
tomekk-hnm <jansen@hnm.ch>
2024-04-24 12:48:08 +0200

    Update gulpfile.js

--- minify/gulpfile.js
+++ minify/gulpfile.js
@@ -1,29 +1,34 @@
-var gulp = require('gulp');
-var uglifyes = require('gulp-uglify-es').default;
-var uglyComposer = require('gulp-uglify/composer');
-var imagemin = require('gulp-imagemin');
-var uglify = uglyComposer(uglifyes, console);
-var exec = require('child_process').exec;
+import gulp from 'gulp';
+import imagemin from 'gulp-imagemin';
+import uglifyes from 'gulp-uglify-es';
+import uglyComposer from 'gulp-uglify/composer.js';
+import exec from 'child_process';
 
-gulp.task('uglify-js', function () {
-	return gulp.src(['../target/public/assets/**/*.js', '!../target/public/assets/**/+(thirdparty|dist)/**', '!../target/public/assets/**/*.js'])
-			.pipe(uglifyes({ keep_classnames: true, keep_fnames: true }))
+var uglify = uglyComposer(uglifyes, console);
+
+
+gulp.task('uglify-js', () => {
+	return gulp.src(['../target/public/assets/**/*.js', '!../target/public/assets/**/+(thirdparty|dist)/**',
+		'!../target/public/assets/vgc/app/**', '!../target/public/assets/**/*.min.js'])
+			.pipe(uglify({ keep_classnames: true, keep_fnames: true }))
 			.pipe(gulp.dest('../target/public/assets'));
 });
 
+
+gulp.task('minify-images', () => {
+	return gulp.src(['../target/public/assets/**/*.png', '../target/public/assets/**/*.jpg', '../target/public/assets/**/*.jpeg'])
+			.pipe(imagemin())
+			.pipe(gulp.dest('../target/public/assets'))
+});
+
+
 gulp.task('minify-css', (cb) => {
 	let cmd = 'npx cleancss --batch --batch-suffix "" "../target/public/assets/**/*.css"';
-	exec(cmd, function (err, stdout, stderr) {
+	return exec.exec(cmd, function (err, stdout, stderr) {
 		console.log(stdout);
 		console.error(stderr);
 		cb(err);
 	});
 });
-
-gulp.task('minify-images', () =>
-	gulp.src(['../target/public/assets/**/*.png', '../target/public/assets/**/*.jpg', '../target/public/assets/**/*.jpeg'])
-		.pipe(imagemin())
-		.pipe(gulp.dest('../target/public/assets'))
-);
 
 gulp.task('minify', gulp.parallel('uglify-js', 'minify-css', 'minify-images'));
