f3c0dbb713f628e86d8989c9520341859d77df2d
tomekk-hnm <jansen@hnm.ch>
2025-02-03 15:42:34 +0100

    Update gulpfile.js

--- minify/gulpfile.js
+++ minify/gulpfile.js
@@ -1,15 +1,13 @@
 import gulp from 'gulp';
 import imagemin from 'gulp-imagemin';
-import terser from 'gulp-terser';
-import exec from 'child_process';
 
 
 
-gulp.task('uglify-js', () => {
-	return gulp.src(['../target/public/assets/**/*.js', '!../target/public/assets/**/+(thirdparty|dist)/**', '!../target/public/assets/**/*.min.js'])
-			.pipe(terser({ keep_classnames: true, keep_fnames: true }))
-			.pipe(gulp.dest('../target/public/assets'));
-});
+// gulp.task('uglify-js', () => {
+// return gulp.src(['../target/public/assets/**/*.js', '!../target/public/assets/**/+(thirdparty|dist)/**', '!../target/public/assets/**/*.min.js'])
+// 		.pipe(terser({ keep_classnames: true, keep_fnames: true }))
+// 		.pipe(gulp.dest('../target/public/assets'));
+// });
 
 
 gulp.task('minify-images', () => {
@@ -19,13 +17,13 @@ gulp.task('minify-images', () => {
 });
 
 
-gulp.task('minify-css', (cb) => {
-	let cmd = 'npx cleancss --batch --batch-suffix "" "../target/public/assets/**/*.css"';
-	return exec.exec(cmd, function (err, stdout, stderr) {
-		console.log(stdout);
-		console.error(stderr);
-		cb(err);
-	});
+// gulp.task('minify-css', (cb) => {
+// let cmd = 'npx cleancss --batch --batch-suffix "" "../target/public/assets/**/*.css"';
+// return exec.exec(cmd, function (err, stdout, stderr) {
+// 	console.log(stdout);
+// 	console.error(stderr);
+// 	cb(err);
+// });
 });
 
-gulp.task('minify', gulp.parallel('uglify-js', 'minify-css', 'minify-images'));
+gulp.task('minify', gulp.series(/*'uglify-js', 'minify-css',*/'minify-images'));
